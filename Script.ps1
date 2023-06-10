888de6ecd33b52ea1a58d7c96729f7e0c06bca7b
Grex98 <39677545+Grex98@users.noreply.github.com>
2023-06-10 23:13:59 +0300

    Update Script.ps1
    Secod version

--- Script.ps1
+++ Script.ps1
@@ -1,23 +1,37 @@
 $TargetFolder = "C:\main"
-$UnLessParam = 1
-while ($UnLessParam = 1)
-{
-	$LastUpdateFile = Get-Childitem $TargetFolder -File -Recurse | sort LastWriteTime -Descending | select -First 1
+#$UnLessParam = 1
+#while ($UnLessParam = 1)
+#{
+	$LastUpdateFile = Get-Childitem $TargetFolder -File -Recurse -exclude *.changed.txt | sort LastWriteTime -Descending | select -First 1
 	$LastTimeUpdatedFile = $LastUpdateFile.LastWriteTime.ToString("dd.MM.yyyy HH:mm:ss")
 	Do
 	{
-		$LastUpdateFile = Get-Childitem $TargetFolder -File -Recurse | sort LastWriteTime -Descending | select -First 1
+		$LastUpdateFile = Get-Childitem $TargetFolder -File -Recurse -exclude *.changed.txt | sort LastWriteTime -Descending | select -First 1
 		$LastTimeUpdatedFileChanget = $LastUpdateFile.LastWriteTime.ToString("dd.MM.yyyy HH:mm:ss")
 	}
 	Until ($LastTimeUpdatedFileChanget -ne $LastTimeUpdatedFile)
 	$LastTimeUpdatedFile = $LastTimeUpdatedFileChanget
-	$FolderOfLastUpdatedFile = $LastUpdateFile.fullName
-	$InfoAboutFile = Get-ChildItem $FolderOfLastUpdatedFile | Format-List -Property * 
+	$NameOfFile = $LastUpdateFile.Name
+	$NameOfDeroctory = $LastUpdateFile.DirectoryName
+	$objshell = New-Object -ComObject Shell.Application
+	$folderObject = $objshell.NameSpace($NameOfDeroctory)
+	$fileObject = $folderObject.ParseName($NameOfFile)
+	$folderObject.GetDetailsOf($fileObject,1)
+	$a = 0
+      		for ( $a ; $a  -le 266; $a++ )
+       		{ 
+         		if( $folderObject.getDetailsOf( $fileObject, $a ))
+           		{
+             			$InfoAboutFile += @{ $( $folderObject.getDetailsOf( $folderObject.items, $a ))  =
+                   		$( $folderObject.getDetailsOf( $fileObject, $a )) }
+           		} 
+       		}  
+     		$InfoAboutFile
 	$InfoAboutFile
 	$DirectoryLastUpdateFile = $LastUpdateFile.DirectoryName
 	$NameUpdateFile = $LastUpdateFile.BaseName
 	$CreationFullName = "$($DirectoryLastUpdateFile)\$($NameUpdateFile).changed.txt"
-	Get-Childitem $FolderOfLastUpdatedFile | Format-List -Property * | Out-File -FilePath $CreationFullName
+	$InfoAboutFile | Out-File -FilePath $CreationFullName
 	Write-Host "It's working"
-	
-}
+	$InfoAboutFile.clear()
+#}
