9544dee0d2a2ad3ee6e268ecc06b4952b0e9fb43
Grex98 <39677545+Grex98@users.noreply.github.com>
2023-06-12 15:43:33 +0300

    Update Script.ps1
    version 3

--- Script.ps1
+++ Script.ps1
@@ -1,7 +1,7 @@
 $TargetFolder = "C:\main"
-#$UnLessParam = 1
-#while ($UnLessParam = 1)
-#{
+$UnLessParam = 1
+while ($UnLessParam = 1)
+{
 	$LastUpdateFile = Get-Childitem $TargetFolder -File -Recurse -exclude *.changed.txt | sort LastWriteTime -Descending | select -First 1
 	$LastTimeUpdatedFile = $LastUpdateFile.LastWriteTime.ToString("dd.MM.yyyy HH:mm:ss")
 	Do
@@ -16,7 +16,6 @@ $TargetFolder = "C:\main"
 	$objshell = New-Object -ComObject Shell.Application
 	$folderObject = $objshell.NameSpace($NameOfDeroctory)
 	$fileObject = $folderObject.ParseName($NameOfFile)
-	$folderObject.GetDetailsOf($fileObject,1)
 	$a = 0
       		for ( $a ; $a  -le 266; $a++ )
        		{ 
@@ -26,12 +25,12 @@ $TargetFolder = "C:\main"
                    		$( $folderObject.getDetailsOf( $fileObject, $a )) }
            		} 
        		}  
-     		$InfoAboutFile
-	$InfoAboutFile
-	$DirectoryLastUpdateFile = $LastUpdateFile.DirectoryName
 	$NameUpdateFile = $LastUpdateFile.BaseName
-	$CreationFullName = "$($DirectoryLastUpdateFile)\$($NameUpdateFile).changed.txt"
+	$CreationFullName = "$($NameOfDeroctory)\$($NameUpdateFile).changed.txt"
 	$InfoAboutFile | Out-File -FilePath $CreationFullName
+	$MyRawString = Get-Content -Raw $CreationFullName
+	$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+	[System.IO.File]::WriteAllLines($CreationFullName, $MyRawString, $Utf8NoBomEncoding)
 	Write-Host "It's working"
 	$InfoAboutFile.clear()
-#}
+}
